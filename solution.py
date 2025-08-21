e148aaa59353dd49811f9d8c06f76c3eb75cacc1
Rajjivani27 <rajjivani027@gmail.com>
2025-08-21 13:29:49 +0530

    Ywork Round-2 Task-2 Solution file

---
+++ solution.py
@@ -0,0 +1,35 @@
+def solution(ratings)-> int:
+    size = len(ratings)
+
+    if size == 1:
+        return 1
+    
+    ans = 0
+    
+    for i in range(0,size):
+        if i == 0:
+            if ratings[i] > ratings[i+1]:
+                ans += 2
+            else:
+                ans += 1
+        elif i==size-1:
+            if ratings[i] > ratings[i-1]:
+                ans += 2
+            else:
+                ans += 1
+        else:
+            if ratings[i] > ratings[i-1] or ratings[i] > ratings[i+1]:
+                ans += 2
+            else:
+                ans += 1
+
+    return ans
+
+
+ratings = list(input("Enter the ratings: ").split(','))
+
+ans = solution(ratings)
+
+print(f"Required Candies = {ans}")
+
+
